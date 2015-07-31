02a79cf42158079862494f6c6b1e7a0ad3e88db9
Andreww8xx8 <avk@8xx8.ru>
2015-07-31 14:48:43 +0000

    Yes-no fixed

--- lib/kirpich/answers.rb
+++ lib/kirpich/answers.rb
@@ -221,11 +221,13 @@ module Kirpich
 
     def yes_no_text(data)
       text = YES_NO.sample
-      text = obr_text(text, 3)
+      text = obr_text(text, 4)
 
       if rand(7) == 0
         text += ". " + response_text(data['text'].gsub(/\?/, ''))
       end
+
+      text
     end
 
     def sin_text
