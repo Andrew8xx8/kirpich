33022e2af4ad1abebb0ec854616a3c2076c78c7e
Andreww8xx8 <avk@8xx8.ru>
2017-09-19 20:50:24 +0400

    Less boobs

--- lib/kirpich/providers/image.rb
+++ lib/kirpich/providers/image.rb
@@ -2,13 +2,13 @@ module Kirpich::Providers
   class Image
     class << self
       def ass_image
-        response = Faraday.get 'http://rails.gusar.1cb9d70e.svc.dockerapp.io/api/images/random'
+        response = Faraday.get 'http://rails.gusar.1cb9d70e.svc.dockerapp.io/api/images/random?q[nsfw_lt]=0.6'
         json = JSON.parse(response.body)
         json['picture_url']
       end
 
       def boobs_image
-        response = Faraday.get 'http://rails.gusar.1cb9d70e.svc.dockerapp.io/api/images/random'
+        response = Faraday.get 'http://rails.gusar.1cb9d70e.svc.dockerapp.io/api/images/random?q[nsfw_lt]=0.6'
         json = JSON.parse(response.body)
         json['picture_url']
       end
